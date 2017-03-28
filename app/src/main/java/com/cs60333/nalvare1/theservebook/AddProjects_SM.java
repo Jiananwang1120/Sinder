ee282a1bf6d0f97a41d9c1f7764f7b225dc84cf9
Natalie Alvarez <nalvare1@nd.edu>
2017-03-28 14:21:09 -0400

    "Version 3b Natalie"

---
+++ app/src/main/java/com/cs60333/nalvare1/theservebook/AddProjects_SM.java
@@ -0,0 +1,16 @@
+package com.cs60333.nalvare1.theservebook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by apple on 3/28/17.
+ */
+
+public class AddProjects_SM extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_sm3_addproject);
+    }
+}
